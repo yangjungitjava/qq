99b9657b555db657360a0c1b1c30edfea6ad55d5
yangjungitjava <815535596@qq.com>
2019-08-27 11:17:43 +0800

    提交到本地仓库

---
+++ src/firstclass.java
@@ -0,0 +1,11 @@
+public class firstclass {
+    public static void main(String[] args) {
+        System.out.println("测试git");
+        System.out.println("测试git");
+        System.out.println("测试git");
+        System.out.println("测试git");
+        System.out.println("测试git");
+        System.out.println("测试git");
+        System.out.println("测试git");
+    }
+}
